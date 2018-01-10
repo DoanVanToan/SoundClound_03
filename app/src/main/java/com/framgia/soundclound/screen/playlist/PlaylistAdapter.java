3f93d0c0a5219191e842971bcb5b63589ad0de04
NamBD <bui.danh.nam@framgia.com>
2018-01-10 16:00:55 +0700

    create tab albumt :

---
+++ app/src/main/java/com/framgia/soundclound/screen/playlist/PlaylistAdapter.java
@@ -0,0 +1,77 @@
+package com.framgia.soundclound.screen.playlist;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.framgia.soundclound.R;
+import com.framgia.soundclound.data.model.Album;
+import com.framgia.soundclound.databinding.ItemAlbumBinding;
+
+import java.util.List;
+
+/**
+ * Created by Bui Danh Nam on 8/1/2018.
+ */
+
+public class PlaylistAdapter extends RecyclerView.Adapter<PlaylistAdapter.ViewHolder> {
+    private List<Album> mAlbums;
+    private OnItemAlbumClick mOnItemAlbumClick;
+
+    public PlaylistAdapter(List<Album> albums) {
+        mAlbums = albums;
+    }
+
+    public List<Album> getAlbums() {
+        return mAlbums;
+    }
+
+    public void updateAlbums(List<Album> albums) {
+        mAlbums = albums;
+        notifyDataSetChanged();
+    }
+
+    public void setOnItemAlbumClick(OnItemAlbumClick onItemAlbumClick) {
+        mOnItemAlbumClick = onItemAlbumClick;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ItemAlbumBinding itemAlbumBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(
+                        parent.getContext()), R.layout.item_album, parent, false);
+
+        return new ViewHolder(itemAlbumBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.bindData(mAlbums.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mAlbums != null ? mAlbums.size() : 0;
+    }
+
+    /**
+     * Created by Bui Danh Nam on 8/1/2018.
+     */
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ItemAlbumBinding mItemAlbumBinding;
+
+        public ViewHolder(ItemAlbumBinding itemAlbumBinding) {
+            super(itemAlbumBinding.getRoot());
+            mItemAlbumBinding = itemAlbumBinding;
+        }
+
+        public void bindData(Album album) {
+            mItemAlbumBinding.setAlbum(album);
+            mItemAlbumBinding.setListener(mOnItemAlbumClick);
+            mItemAlbumBinding.executePendingBindings();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/framgia/soundclound/screen/playlist/PlaylistModelView.java
@@ -0,0 +1,161 @@
+package com.framgia.soundclound.screen.playlist;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.framgia.soundclound.BR;
+import com.framgia.soundclound.R;
+import com.framgia.soundclound.data.model.Album;
+import com.framgia.soundclound.data.source.repository.AlbumRepository;
+
+/**
+ * Created by Bui Danh Nam on 8/1/2018.
+ */
+
+public class PlaylistModelView extends BaseObservable implements OnItemAlbumClick {
+    private PlaylistAdapter mPlaylistAdapter;
+    private Context mContext;
+
+    public PlaylistModelView(Context context) {
+        mContext = context;
+        mPlaylistAdapter = new PlaylistAdapter(
+                AlbumRepository.getInstance(mContext).getAllAlbum());
+        mPlaylistAdapter.setOnItemAlbumClick(this);
+    }
+
+    @Bindable
+    public PlaylistAdapter getPlaylistAdapter() {
+        return mPlaylistAdapter;
+    }
+
+    public void setPlaylistAdapter(PlaylistAdapter playlistAdapter) {
+        mPlaylistAdapter = playlistAdapter;
+        notifyPropertyChanged(BR.playlistAdapter);
+    }
+
+    public void onFabAddAlbumClick() {
+        showDialogAddAlbum();
+    }
+
+    private void showDialogAddAlbum() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setTitle(R.string.title_add_album);
+        builder.setMessage(R.string.msg_name_album);
+        final EditText input = new EditText(builder.getContext());
+        builder.setView(input);
+        builder.setPositiveButton(R.string.action_add, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String nameAlbum = input.getText().toString().trim();
+                if (nameAlbum.isEmpty()) {
+                    Toast.makeText(
+                            mContext, R.string.msg_err_name_null, Toast.LENGTH_SHORT).show();
+                    dialog.cancel();
+                }
+                Album albumTemp = new Album();
+                albumTemp.setName(nameAlbum);
+                boolean result = AlbumRepository.getInstance(mContext).addAlbum(albumTemp);
+                if (result) {
+                    updateUI();
+                } else {
+                    Toast.makeText(
+                            mContext, R.string.msg_err_name_exist, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        builder.setNegativeButton(R.string.action_cancel, null);
+        builder.show();
+    }
+
+    private void updateUI() {
+        mPlaylistAdapter.updateAlbums(
+                AlbumRepository.getInstance(mContext).getAllAlbum());
+    }
+
+    @Override
+    public void onItemClick(Album album) {
+        //// TODO: 10/1/2018  click item
+    }
+
+    @Override
+    public void onItemMoreClick(ImageView imageView, final Album album) {
+        if (album == null) {
+            return;
+        }
+        final AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        PopupMenu popupMenu = new PopupMenu(mContext, imageView);
+        popupMenu.getMenuInflater().inflate(R.menu.popup_menu_album, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.delete_album:
+                        handleDeleteAlbum(album, builder);
+                        break;
+                    case R.id.rename_album:
+                        handleRenameAlbum(album, builder);
+                        break;
+                }
+                builder.show();
+                return true;
+            }
+        });
+        popupMenu.show();
+    }
+
+    private void handleRenameAlbum(final Album album, AlertDialog.Builder builder) {
+        builder.setTitle(R.string.msg_rename_album);
+        builder.setMessage(R.string.msg_name_album);
+        final EditText inputNameAlbum = new EditText(builder.getContext());
+        builder.setView(inputNameAlbum);
+        inputNameAlbum.setText(album.getName());
+        builder.setPositiveButton(
+                R.string.action_ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String nameAlbum = inputNameAlbum.getText().toString().trim();
+                        if (nameAlbum.isEmpty()) {
+                            Toast.makeText(mContext,
+                                    R.string.msg_err_name_null,
+                                    Toast.LENGTH_SHORT).show();
+                            dialog.cancel();
+                        }
+                        if (nameAlbum.equals(album.getName())) {
+                            dialog.cancel();
+                        }
+                        album.setName(nameAlbum);
+                        boolean resultRename = AlbumRepository
+                                .getInstance(mContext).renameAlbum(album);
+                        if (resultRename) {
+                            updateUI();
+                        }
+                    }
+                });
+        builder.setNegativeButton(R.string.action_cancel, null);
+    }
+
+    private void handleDeleteAlbum(final Album album, AlertDialog.Builder builder) {
+        builder.setTitle(R.string.msg_delele_album);
+        builder.setPositiveButton(R.string.action_ok,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        boolean resultDelAlbum = AlbumRepository.getInstance(mContext)
+                                .deleteAlbum(album);
+                        if (resultDelAlbum) {
+                            mPlaylistAdapter.updateAlbums(
+                                    AlbumRepository.getInstance(mContext).getAllAlbum());
+                        }
+                    }
+                });
+        builder.setNegativeButton(R.string.action_cancel, null);
+    }
+}
