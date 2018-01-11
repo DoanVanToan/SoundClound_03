df6a10a586ee5b7b084a0a3796f9de77687bfcee
DESKTOP-LPVM6O0\Sony <nguyenduc0496@gmail.com>
2018-01-11 10:05:31 +0700

    addplaytrack

---
+++ app/src/main/java/com/framgia/soundclound/screen/playtrack/PlayTrackActivity.java
@@ -0,0 +1,18 @@
+package com.framgia.soundclound.screen.playtrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.framgia.soundclound.R;
+
+/**
+ * Created by Sony on 1/11/2018.
+ */
+public class PlayTrackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_track);
+    }
+}
